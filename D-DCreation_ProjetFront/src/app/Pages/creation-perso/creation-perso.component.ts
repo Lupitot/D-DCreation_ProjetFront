4ef18221256936ceb9634ac1a7408313ad845c91
Lupitot <paul.piauger2@gmail.com>
2024-03-09 10:59:20 +0100

    creation dossier de base

---
+++ D-DCreation_ProjetFront/src/app/Pages/creation-perso/creation-perso.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creation-perso',
+  standalone: true,
+  imports: [],
+  templateUrl: './creation-perso.component.html',
+  styleUrl: './creation-perso.component.scss'
+})
+export class CreationPersoComponent {
+
+}
---
+++ D-DCreation_ProjetFront/src/app/components/form-perso/form-perso.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-perso',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-perso.component.html',
+  styleUrl: './form-perso.component.scss'
+})
+export class FormPersoComponent {
+
+}
